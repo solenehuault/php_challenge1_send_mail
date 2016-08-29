ed8447a6e5546f8866da4f5546cc62e550064b4a
solenehuault <solene.huault@gmail.com>
2016-08-29 11:53:18 +0200

    send mail succed

--- index.php
+++ index.php
@@ -3,29 +3,25 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Send mail</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-		<h1>Send mail</h1>
 		<form method="post" action="index.php">
-			<input type="email" name="email" placeholder="your@email.com"></input><br />
-			<input type="text" name="subject" placeholder="Subject"></input><br />
-			<textarea type="textarea" name="message" placeholder="Your message" rows="20" cols="40"></textarea><br />
-			<input type="submit" value="Send"></input>
+			<fieldset>
+				<legend>Send Mail</legend>
+				<input type="email" name="email" placeholder="your@email.com"></input><br />
+				<input type="text" name="subject" placeholder="Subject"></input><br />
+				<textarea type="textarea" name="message" placeholder="Your message" rows="20" cols="40" maxlength="500"></textarea><br />
+				<input type="submit" value="Send"></input>
+			</fieldset>
 		</form>
 
 		<?php
-
-		$to      = 'vv221@dotslashplay.it';
-		$subject = 'Test1';
-		$message = 'Blebleble';
-		$headers = 'From: mopi@dotslashplay.it' . "\r\n" .
-		    'Reply-To: mopi@dotslashplay.it' . "\r\n" .
-				    'X-Mailer: PHP/' . phpversion();
-			//if (isset($_POST['email']) &&
-			//		isset($_POST['message'])&&
-			//		isset($_POST['subject'])) {
-				mail($to, $subject, $message, $headers);
-			//}
+			if (isset($_POST['email']) &&
+					isset($_POST['message'])&&
+					isset($_POST['subject'])) {
+				mail($_POST['email'], $_POST['subject'], $_POST['message']);
+			}
 		?>
 	</body>
 </html>
